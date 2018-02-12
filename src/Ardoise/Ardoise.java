01ff48912db25f4354b9cd79664c237e253082e4
Arthur <arthur.langenbronn@gmail.com>
2018-02-12 13:28:08 +0100

    Version 1 : Ardoise:	couleur : rouge,vert, bleu 			forme : rond, carré

--- src/Ardoise/Ardoise.java
+++ src/Ardoise/Ardoise.java
@@ -15,7 +15,6 @@ public class Ardoise extends JPanel {
 	private int pointerSize = 10;
 	private String pointerType = "ROND";
 	private ArrayList<Point> points = new ArrayList<Point>();
-	private boolean erasing = true;
 	
 	public Ardoise(){
 	    this.addMouseListener(new MouseAdapter(){
@@ -32,36 +31,22 @@ public class Ardoise extends JPanel {
 	      public void mouseMoved(MouseEvent e) {}
 	    });
 	  }   
-	  // Vous la connaissez maintenant, celle-là
 	  public void paintComponent(Graphics g) {
-
 	    g.setColor(Color.white);
 	    g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
-	    //Si on doit effacer, on ne passe pas dans le else => pas de dessin
-	    if(this.erasing){
-	      this.erasing = false;
-	    }
-	    else{
-	      //On parcourt notre collection de points
 	      for(Point p : this.points)
 	      {
-	        //On récupère la couleur
 	        g.setColor(p.getColor());
-
-	        //Selon le type de point
 	        if(p.getType().equals("SQUARE")){
 	          g.fillRect(p.getX(), p.getY(), p.getSize(), p.getSize());
 	        }
 	        else{
 	          g.fillOval(p.getX(), p.getY(), p.getSize(), p.getSize());
 	        }
-	      }
-	    }        
+	      }     
 	  }
 
 	  public void erase(){
-	    this.erasing = true;
 	    this.points = new ArrayList<Point>();
 	    repaint();
 	  }
--- src/Ardoise/Fenetre.java
+++ src/Ardoise/Fenetre.java
@@ -29,9 +29,7 @@ public class Fenetre extends JFrame{
 					  couleurRouge = new JMenuItem("Rouge"),
 					  couleurVert = new JMenuItem("Vert"),
 					  couleurBleu = new JMenuItem("Bleu");
-
     private JToolBar toolBar = new JToolBar();
-
     private JButton carre = new JButton(new ImageIcon("images/carré.png")),
     				rond = new JButton(new ImageIcon("images/rond.png")),
     				red = new JButton(new ImageIcon("images/red.png")),
@@ -39,7 +37,6 @@ public class Fenetre extends JFrame{
     				blue = new JButton(new ImageIcon("images/blue.png"));
     private Color fondBouton = Color.white;
 	private Ardoise ardoise = new Ardoise();
-
 
 	  public static void main(String[] args){
 		  Fenetre fen = new Fenetre();
@@ -52,47 +49,82 @@ public class Fenetre extends JFrame{
 	    this.InitMenu();
 	    this.initToolBar();
 	    this.getContentPane().add(ardoise, BorderLayout.CENTER);  
+	    this.setVisible(true);
+	}
+	public void InitMenu(){
+	    this.menuFichier.add(effacer);
+	    effacer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ardoise.erase();			
+			}
+		});
+	    this.menuFichier.addSeparator();
+	    this.menuFichier.add(quitter);
 	    quitter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 				}
 		});
-	    this.setVisible(true);
-	}
-	public void InitMenu(){
-	    this.menuFichier.add(effacer);
-	    this.menuFichier.addSeparator();
-	    this.menuFichier.add(quitter);
 	    this.menuEdition.add(menuForme);
 	    this.menuForme.add(formeRond);
 	    this.menuForme.add(formeCarré);
+	    formeRond.addActionListener(new FormeListener());
+	    formeCarré.addActionListener(new FormeListener());  
 	    this.menuEdition.addSeparator();
 	    this.menuEdition.add(menuCouleur);
 	    this.menuCouleur.add(couleurRouge);
 	    this.menuCouleur.add(couleurVert);	    
 	    this.menuCouleur.add(couleurBleu);
+	    couleurRouge.addActionListener(new CouleurListener());
+	    couleurVert.addActionListener(new CouleurListener());
+	    couleurBleu.addActionListener(new CouleurListener());
 	    this.menuBar.add(menuFichier);
 	    this.menuBar.add(menuEdition);
 	    this.setJMenuBar(menuBar);
 	}
-	  private void initToolBar(){
-		    this.carre.setEnabled(false);
-		    this.carre.setBackground(fondBouton);
-		    this.toolBar.add(carre);
-		    this.rond.setEnabled(false);
-		    this.rond.setBackground(fondBouton);
-		    this.toolBar.add(rond);
-		    
-		    this.red.setEnabled(false);
-		    this.red.setBackground(fondBouton);
-		    this.toolBar.add(red);
-		    this.green.setEnabled(false);
-		    this.green.setBackground(fondBouton);
-		    this.toolBar.add(green);
-		    this.blue.setEnabled(false);
-		    this.blue.setBackground(fondBouton);
-		    this.toolBar.add(blue);
-		    
-		    this.getContentPane().add(toolBar, BorderLayout.NORTH);    
+	  private void initToolBar(){  		  
+		  this.carre.setBackground(fondBouton);		  
+		  this.toolBar.add(carre);		  		  
+		  this.rond.setBackground(fondBouton);		  
+		  this.toolBar.add(rond);
+		  carre.addActionListener(new FormeListener());
+		  rond.addActionListener(new FormeListener());    
+		  this.red.setBackground(fondBouton);
+		  this.toolBar.add(red);
+		  this.green.setBackground(fondBouton);
+		  this.toolBar.add(green);
+		  this.blue.setBackground(fondBouton);
+		  this.toolBar.add(blue);
+		  red.addActionListener(new CouleurListener());
+		  green.addActionListener(new CouleurListener());
+		  blue.addActionListener(new CouleurListener());
+		   
+		  this.getContentPane().add(toolBar, BorderLayout.NORTH);    		 
+	  }
+	  class FormeListener implements ActionListener{
+		    public void actionPerformed(ActionEvent e) {
+		      if(e.getSource().getClass().getName().equals("javax.swing.JMenuItem")){
+		        if(e.getSource()==formeCarré) ardoise.setPointerType("SQUARE");
+		        else ardoise.setPointerType("CIRCLE");
+		      }
+		      else{
+		        if(e.getSource()==carre)ardoise.setPointerType("SQUARE");
+		        else ardoise.setPointerType("CIRCLE");        
+		      }
+		    }    
 		  }
+	  class CouleurListener implements ActionListener{
+		    public void actionPerformed(ActionEvent e) {
+		        if(e.getSource().getClass().getName().equals("javax.swing.JMenuItem")){
+		          if(e.getSource()==couleurVert)ardoise.setPointerColor(Color.green);
+		          else if(e.getSource()==couleurBleu)ardoise.setPointerColor(Color.blue);
+		          else ardoise.setPointerColor(Color.red);
+		        }
+		        else{
+		          if(e.getSource()==green)ardoise.setPointerColor(Color.green);
+		          else if(e.getSource()==blue)ardoise.setPointerColor(Color.blue);
+		          else ardoise.setPointerColor(Color.red);        
+		        }
+		      }    
+		    }
 }
